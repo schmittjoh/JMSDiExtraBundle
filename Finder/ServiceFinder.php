0526a33886188dcf4e55b2996a983162bf17fde9
Johannes Schmitt <schmittjoh@gmail.com>
2011-05-10 18:38:49 +0200

    [Finder] added FINDSTR support for Windows

--- Finder/ServiceFinder.php
+++ Finder/ServiceFinder.php
@@ -2,6 +2,7 @@
 
 namespace JMS\DiExtraBundle\Finder;
 
+use Symfony\Component\Finder\Finder;
 use JMS\DiExtraBundle\Exception\RuntimeException;
 use Symfony\Component\Process\ExecutableFinder;
 
@@ -24,13 +25,37 @@ class ServiceFinder
             return $this->findUsingGrep($bundles);
         }
 
-        // TODO: Add FINDSTR support for Windows
+        // use FINDSTR on Windows
+        if (false !== stripos(PHP_OS, 'win')) {
+            return $this->findUsingFindstr($bundles);
+        }
 
         // this should really be avoided at all costs since it is damn slow
         return $this->findUsingFinder($bundles);
     }
 
-    private function findUsingGrep(array $bundles, $grepPath)
+    private function findUsingFindstr(array $bundles)
+    {
+        $dirs = '';
+        foreach ($bundles as $bundle) {
+            $dirs .= ';'.$bundle->getPath();
+        }
+
+        $cmd = 'FINDSTR /M /S /L';
+        $cmd .= ' /D:'.escapeshellarg(substr($dirs, 1));
+        $cmd .= ' '.escapeshellarg(self::PATTERN);
+        $cmd .= ' *.php';
+
+        exec($cmd, $files, $exitCode);
+
+        if (0 !== $exitCode) {
+            throw new \RuntimeException(sprintf('Command "%s" exited with non-successful status code. "%d".', $cmd, $exitCode));
+        }
+
+        return $files;
+    }
+
+    private function findUsingGrep(array $bundles)
     {
         $cmd = $this->grepPath.' --fixed-strings --directories=recurse --devices=skip --files-with-matches --with-filename --max-count=1 --color=never --exclude-dir=\.git --exclude-dir=Resources --include=*.php';
         $cmd .= ' '.escapeshellarg(self::PATTERN);
@@ -50,6 +75,7 @@ class ServiceFinder
     private function findUsingFinder(array $bundles)
     {
         $finder = new Finder();
+        $pattern = self::PATTERN;
         $finder
             ->files()
             ->name('*.php')
@@ -58,8 +84,8 @@ class ServiceFinder
                 }, $bundles
             ))
             ->ignoreVCS(true)
-            ->filter(function($file) {
-                return false !== strpos(file_get_contents($file->getPathName()), self::PATTERN);
+            ->filter(function($file) use ($pattern) {
+                return false !== strpos(file_get_contents($file->getPathName()), $pattern);
             })
         ;
 
---
+++ Tests/PerformanceTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace JMS\DiExtraBundle\Tests;
+
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Bundle\DoctrineBundle\DoctrineBundle;
+use JMS\DiExtraBundle\JMSDiExtraBundle;
+use JMS\SecurityExtraBundle\JMSSecurityExtraBundle;
+use Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle;
+use Symfony\Bundle\FrameworkBundle\DependencyInjection\FrameworkExtension;
+use Symfony\Bundle\AsseticBundle\AsseticBundle;
+use Symfony\Bundle\MonologBundle\MonologBundle;
+use Symfony\Bundle\SecurityBundle\SecurityBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use JMS\DiExtraBundle\Finder\ServiceFinder;
+
+class PerformanceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider getFinderMethods
+     */
+    public function testServiceFinder($method)
+    {
+        $finder = new ServiceFinder();
+        $ref = new \ReflectionMethod($finder, $method);
+        $ref->setAccessible(true);
+
+        $bundles = array(
+            new FrameworkBundle(),
+            new SecurityBundle(),
+            new MonologBundle(),
+            new AsseticBundle(),
+            new DoctrineBundle(),
+            new TwigBundle(),
+            new SensioFrameworkExtraBundle(),
+            new JMSSecurityExtraBundle(),
+            new JMSDiExtraBundle(),
+        );
+
+        $time = microtime(true);
+        for ($i=0,$c=5; $i<$c; $i++) {
+            $ref->invoke($finder, $bundles);
+        }
+        $time = microtime(true) - $time;
+        $this->printResults('service finder ('.$method.')', $time, $c);
+    }
+
+    public function getFinderMethods()
+    {
+        return array(
+            array('findUsingGrep'),
+            array('findUsingFinder'),
+        );
+    }
+
+    private function printResults($test, $time, $iterations)
+    {
+        if (0 == $iterations) {
+            throw new InvalidArgumentException('$iterations cannot be zero.');
+        }
+
+        $title = $test." results:\n";
+        $iterationsText = sprintf("Iterations:         %d\n", $iterations);
+        $totalTime      = sprintf("Total Time:         %.3f s\n", $time);
+        $iterationTime  = sprintf("Time per iteration: %.3f ms\n", $time/$iterations * 1000);
+
+        $max = max(strlen($title), strlen($iterationTime)) - 1;
+
+        echo "\n".str_repeat('-', $max)."\n";
+        echo $title;
+        echo str_repeat('=', $max)."\n";
+        echo $iterationsText;
+        echo $totalTime;
+        echo $iterationTime;
+        echo str_repeat('-', $max)."\n";
+    }
+}
