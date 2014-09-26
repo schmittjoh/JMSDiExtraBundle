ed40cc7a319fc854608cc24fbe24391285637f77
sidux <ahmed.lebbada@gmail.com>
2014-09-26 11:37:00 +0200

    make class name check case insensitive

--- DependencyInjection/Compiler/AnnotationConfigurationPass.php
+++ DependencyInjection/Compiler/AnnotationConfigurationPass.php
@@ -111,7 +111,7 @@ class AnnotationConfigurationPass implements CompilerPassInterface
         }
         $namespace = $match[1];
 
-        if (!preg_match('/\bclass\s+([^\s]+)\s+(?:extends|implements|{)/s', $src, $match)) {
+        if (!preg_match('/\bclass\s+([^\s]+)\s+(?:extends|implements|{)/is', $src, $match)) {
             throw new RuntimeException(sprintf('Could not extract class name from file "%s".', $filename));
         }
 
