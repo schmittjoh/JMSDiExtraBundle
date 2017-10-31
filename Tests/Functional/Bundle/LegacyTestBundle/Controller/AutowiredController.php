8c6da9d618f1ee9b6f9ab84017fe55257be95da0
Ruben Smets <smets.ruben@gmail.com>
2017-10-31 15:02:37 +0100

    Issue#282: add Controller to LegacyTestBundle for older versions

---
+++ Tests/Functional/Bundle/LegacyTestBundle/Controller/AutowiredController.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Copyright 2011 Johannes M. Schmitt <schmittjoh@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace JMS\DiExtraBundle\Tests\Functional\Bundle\LegacyTestBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AutowiredController extends Controller
+{
+    /**
+     * @Route("/autowired-controller-test")
+     */
+    public function testAction()
+    {
+        $content = '';
+        $content .= sprintf("\$has container: %s\n", $this->container !== null ? 'OK' : 'FAILED');
+
+        return new Response($content);
+    }
+}
